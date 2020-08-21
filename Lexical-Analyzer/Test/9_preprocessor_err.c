c7c8369aae5b7bf28abfcff9b85968bfac2b379e
Omanshu480 <omanshumahawar.181co237@nitk.edu.in>
2020-08-22 00:26:30 +0530

    Pre-Processor Error test file

---
+++ Lexical-Analyzer/Test/9_preprocessor_err.c
@@ -0,0 +1,12 @@
+#incl < stdio.h>
+
+//Testing String Error
+
+void main()
+{
+  int age = 20;
+  char firstname = "Omanshu";
+  char lastname = "Mahawar;
+
+      printf("%s %s\n", firstname, lastname);
+}
